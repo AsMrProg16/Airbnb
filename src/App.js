1ef8582ed25036ee0bf854a8e35b01299432187e
AsMrProg16 <asmrprog16@gmail.com>
2023-10-11 16:55:02 +0500

    loginpages start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import IndexPage from './pages/indexpage'
+import LoginPage from './pages/loginpage'
 function App() {
   return (
     <Routes>
       <Route index element={<IndexPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
 
   );
---
+++ src/pages/loginpage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loginpage() {
+  return (
+    <>
+          login pages start code ....
+    </>
+  )
+}
